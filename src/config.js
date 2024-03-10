d81f2a375da5863aee3824507665f7e5966156e7
irfan <irfan041103@gmail.com>
2024-03-10 09:53:40 +0700

    fix: base url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const url = "https://api.ayochat.my.id"
-export const url = "http://localhost:3000"
+export const url = "https://api.ayochat.my.id"
+// export const url = "http://localhost:3000"
